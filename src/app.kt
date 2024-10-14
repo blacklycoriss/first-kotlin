c7a3c206e87a154168cb321e5f35b9d6b9aaf9b6
BlackLycoriss <prokopovaleksandr544@gmail.com>
2024-10-15 03:11:00 +0700

    primitive auth

--- src/app.kt
+++ src/app.kt
@@ -1,4 +1,28 @@
 fun main()
 {
-    println("Hello, world");
+    print("Введите логин: ");
+    val login = readLine();
+
+    print("Введите пароль: ");
+    val password = readLine();
+
+    val message: String;
+
+    if (login == "alex228")
+    {
+        if (password == "dmm041021")
+        {
+            message = "Доступ разрешен.";
+        }
+        else
+        {
+            message = "Доступ запрещен. Неверный пароль";
+        }
+    }
+    else
+    {
+        message = "Доступ запрещен. Неверный логин.";
+    }
+
+    println(message);
 }
